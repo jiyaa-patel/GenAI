b48ebca398c69c1d782ee7c7c167c06fd612c227
Rachit288 <rachit2808@gmail.com>
2025-09-21 22:29:20 +0530

    Removed unnecessary files

--- backEnd/geniai/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeniaiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geniai'
-    verbose_name = 'GenAI Document Management'
